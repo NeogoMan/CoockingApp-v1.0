cf1fd60dd4e5cbbf1b5d0d77bea816daf228b48a
NeogoMan <neogoapp@gmail.com>
2016-08-08 11:19:37 +0100

    Recipe Api Key

--- app/src/main/java/recipe/coking/kitchen/myrecipeapp/Utils/AppConstant.java
+++ app/src/main/java/recipe/coking/kitchen/myrecipeapp/Utils/AppConstant.java
@@ -8,5 +8,5 @@ public class AppConstant {
     public static String    BASE_URL         = "http://food2fork.com/api/";
 
     //get Key from http://food2fork.com/about/api
-    public final  static String    KEY       = "c20ba2ed57e6928b656b2f747d86abc7";
+    public final  static String    KEY       = "Put your Api Key";
 }
